8b3e61a5056d48f5fd95b3942d0603f520412480
Philipp Arndt <2f.mail@gmx.de>
2025-08-04 14:59:29 +0200

    chore(deps): update tailwind to 4.x

---
+++ app/web/tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+} satisfies Config
